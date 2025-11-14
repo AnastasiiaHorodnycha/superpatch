5eee5ef60aaaee76b2aaad1a9df40365bbf85ec7
gorodnichaya-av <gorodnichaia.av@gmail.com>
2025-11-14 12:45:17 +0200

    finished

--- js/script.js
+++ js/script.js
@@ -1,24 +1,10 @@
-/**
- * Add class to Header on Scroll
- */
-window.addEventListener('scroll', function () {
-  const header = document.querySelector('.header');
-  if (!header) return;
-  if (window.scrollY > 0) {
-    header.classList.add('scrolled');
-  } else {
-    header.classList.remove('scrolled');
-  }
-});
 
-console.log('dddd');
-
-
-
-/**
- * Smooth scroll to anchor links on the page
- */
 document.addEventListener('DOMContentLoaded', function () {
+
+
+    /**
+   * Smooth scroll to anchor links on the page
+   */
     document.querySelectorAll('.js-anchor').forEach(anchor => {
         anchor.addEventListener('click', function (e) {
             const targetId = this.getAttribute('href').slice(1);
@@ -31,84 +17,126 @@ document.addEventListener('DOMContentLoaded', function () {
             }
         });
     });
-});
 
-/**
- * Download PDF
- */
-document.querySelectorAll('.js-download-pdf').forEach(btn => {
-  btn.addEventListener('click', function (e) {
-    e.preventDefault();
-    const pdfUrl = this.getAttribute('data-pdf-url');
-    if (pdfUrl) {
-      const link = document.createElement('a');
-      link.href = pdfUrl;
-      link.download = '';
-      document.body.appendChild(link);
-      link.click();
-      document.body.removeChild(link);
-    }
+    
+  /**
+   * Download PDF
+   */
+  document.querySelectorAll('.js-download-pdf').forEach(btn => {
+    btn.addEventListener('click', function (e) {
+      e.preventDefault();
+      const pdfUrl = this.getAttribute('data-pdf-url');
+      if (pdfUrl) {
+        const link = document.createElement('a');
+        link.href = pdfUrl;
+        link.download = '';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+      }
+    });
   });
-});
 
-/**
- * Accordion Functionality
- */
-const accordionHeaders = document.querySelectorAll('.js-accordion-btn');
 
-accordionHeaders.forEach(header => {
-  header.addEventListener('click', () => {
-    const content = header.nextElementSibling;
-    const open = header.classList.contains('active');
+    /**
+   * Accordion Functionality
+   */
+  const accordionHeaders = document.querySelectorAll('.js-accordion-btn');
 
-    document.querySelectorAll('.js-accordion-content').forEach(c => {
-      c.style.height = 0;
-      c.previousElementSibling.classList.remove('active');
+  accordionHeaders.forEach(header => {
+    header.addEventListener('click', () => {
+      const content = header.nextElementSibling;
+      const open = header.classList.contains('active');
+
+      document.querySelectorAll('.js-accordion-content').forEach(c => {
+        c.style.height = 0;
+        c.previousElementSibling.classList.remove('active');
+      });
+
+      if (!open) {
+        header.classList.add('active');
+        content.style.height = content.scrollHeight + 'px';
+      }
+    });
+  });
+
+  /**
+   * Modal Functionality
+   */
+  document.querySelectorAll('.js-modal-open').forEach(btn => {
+    btn.addEventListener('click', function (e) {
+      e.preventDefault();
+      const modalPdf = this.getAttribute('data-pdf-url'),
+            modalImg = this.getAttribute('data-img-url'),
+            modal = document.querySelector('.js-modal');
+      if (modal) {
+        modal.classList.add('active');
+        modal.querySelector('.js-modal-poster').src = modalImg;
+        modal.querySelector('.js-download-pdf').dataset.pdfUrl = modalPdf;
+        document.body.classList.add('modal-open');
+      }
+    });
+  });
+
+  document.querySelectorAll('.js-modal-close').forEach(btn => {
+    btn.addEventListener('click', function (e) {
+      e.preventDefault();
+      const modal = this.closest('.modal');
+      if (modal) {
+        modal.classList.remove('active');
+        document.body.classList.remove('modal-open');
+      }
+    });
+  });
+
+  // Close modal when clicking outside modal content
+  document.querySelectorAll('.js-modal').forEach(modal => {
+    modal.addEventListener('click', function (e) {
+      if (e.target === modal) {
+        modal.classList.remove('active');
+        document.body.classList.remove('modal-open');
+      }
+    });
+  });
+
+
+
+  
+
+  document.getElementById('js-form').addEventListener('submit', function(e) {
+    e.preventDefault(); // блокує оновлення сторінки
+
+    const textInputs = document.querySelectorAll('.js-field');
+    let isValid = true;
+
+    textInputs.forEach(input => {
+      if (input.value.trim() === '') {
+        input.parentElement.classList.add('error');
+        isValid = false;
+      } else {
+        input.parentElement.classList.remove('error');
+      }
     });
 
-    if (!open) {
-      header.classList.add('active');
-      content.style.height = content.scrollHeight + 'px';
+    if (isValid) {
+      document.querySelector('.js-form-wrap').style.display = "none";
+      document.querySelector('.js-form-success').style.display = "block";
     }
   });
+
+  const textInputs = document.querySelectorAll('.js-field');
+  textInputs.forEach(input => {
+    input.addEventListener('input', () => {
+      input.parentElement.classList.remove('error');
+    });
+  });
+
+
+
+  
 });
 
-/**
- * Modal Functionality
- */
-document.querySelectorAll('.js-modal-open').forEach(btn => {
-  btn.addEventListener('click', function (e) {
-    e.preventDefault();
-    const modalPdf = this.getAttribute('data-url-pdf'),
-          modalImg = this.getAttribute('data-img-url'),
-          modal = document.querySelector('.js-modal');
-    if (modal) {
-      modal.classList.add('active');
-      modal.querySelector('.js-modal-img').src = modalImg;
-      modal.querySelector('.js-download-pdf').dataset.dataPdfUrl = modalPdf;
-      document.body.classList.add('modal-open');
-    }
-  });
-});
 
-document.querySelectorAll('.js-modal-close').forEach(btn => {
-  btn.addEventListener('click', function (e) {
-    e.preventDefault();
-    const modal = this.closest('.modal');
-    if (modal) {
-      modal.classList.remove('active');
-      document.body.classList.remove('modal-open');
-    }
-  });
-});
 
-// Close modal when clicking outside modal content
-document.querySelectorAll('.js-modal').forEach(modal => {
-  modal.addEventListener('click', function (e) {
-    if (e.target === modal) {
-      modal.classList.remove('active');
-      document.body.classList.remove('modal-open');
-    }
-  });
-});
+
 
